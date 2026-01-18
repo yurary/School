b81c2411d06d6043d5fc698354ee9a76a7cdc8f0
yurary <yura.rus2020@gmail.com>
2026-01-19 00:14:35 +0100

    add new javaa folder

---
+++ exams_inf0.3/Szkolenia/index.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html lang="PL">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Szkolenia i kursy</title>
+    <link rel="stylesheet" href="styl.css">
+</head>
+<body>
+    <header><h1>SZKOLENIA</h1></header>
+    <main>
+        <section>
+            <table>
+                <td>asdasd</td><td>asd</td><td>qwe</td>
+            </table>
+        </section>
+        <section></section>
+    </main>
+    <footer></footer>
+    
+</body>
+</html>
